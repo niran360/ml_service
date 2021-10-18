fc6f77f4fdc6fb24ca528ed2a6093fb401ffa889
Your Name <niran360@gmail.com>
2021-10-18 19:02:51 +0100

    test case

--- backend/server/apps/ml/income_classifier/random_forest.py
+++ backend/server/apps/ml/income_classifier/random_forest.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 class RandomForestClassifier:
     def __init__(self):
-        path_to_artifacts = "../../research"
+        path_to_artifacts = "../../research/"
         self.values_fill_missing = joblib.load(path_to_artifacts + "train_mode.joblib")
         self.encoders = joblib.load(path_to_artifacts +"encoders.joblib")
         self.model = joblib.load(path_to_artifacts + "random_forest.joblib")
@@ -14,7 +14,7 @@ class RandomForestClassifier:
         #fill missing values
         input_data.fillna(self.values_fill_missing)
         # convert categorical values
-        for columns in [
+        for column in [
             "workclass",
             "education",
             "marital_status",
@@ -25,24 +25,24 @@ class RandomForestClassifier:
             "native_country",
         ]:
             categorical_convert = self.encoders[column]
-            input_data[column] =categorical_convert,transform(input_data[column])
+            input_data[column] =categorical_convert.transform(input_data[column])
         
         return input_data
 
     def predict(self, input_data):
         return self.model.predict_proba(input_data)
 
-    def preprocessing(self, input_data):
+    def postprocessing(self, input_data):
         label = "<=50"
         if input_data[1] > 0.5:
             label = ">50k"
-        return {"probability": input_data[1], "label" :label, "status": "OK"}
+        return {"probability": input_data[1], "label": label, "status": "OK"}
 
     def compute_prediction(self, input_data):
         try:
             input_data = self.preprocessing(input_data)
             prediction = self.predict(input_data)[0]
-            prediction = self.preprocessing(prediction)
+            prediction = self.postprocessing(prediction)
         except Exception as e:
             return {"status": "Error", "message": str(e)}
 
