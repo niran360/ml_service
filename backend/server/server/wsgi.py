3d46a9716e951b9f8ccc782ee009c8401be21088
Your Name <niran360@gmail.com>
2021-10-18 19:43:25 +0100

    ml registry & wsgi

--- backend/server/server/wsgi.py
+++ backend/server/server/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for server project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +5,23 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
 application = get_wsgi_application()
+
+import inspect
+from apps.ml.registry import MLRegistry
+from apps.ml.income_classifier.random_forest import RandomForestClassifier
+
+try:
+    registry = MLRegistry()
+    rf = RandomForestClassifier()
+
+    registry.add_algorithm(endpoint_name = "income_classifier",
+    algorithm_object = rf,
+    algorithm_name = "random forest",
+    algorithm_status = "production",
+    algorithm_version = "0.0.1",
+    owner = "Ayo",
+    algorithm_description = "Random Forest with sample pre and post-processing",
+    algorithm_code=inspect.getsource(RandomForestClassifier))
+
+except Exception as e:
+    print("Exception while loading the algorithm to the registry,", str(e))
