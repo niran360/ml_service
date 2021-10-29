74265ed02bce574e10d64ba2b7053a7bf48c6a48
Your Name <niran360@gmail.com>
2021-10-29 23:45:07 +0100

    ab test

--- backend/server/server/wsgi.py
+++ backend/server/server/wsgi.py
@@ -6,6 +6,7 @@ application = get_wsgi_application()
 import inspect
 from apps.ml.registry import MLRegistry
 from apps.ml.income_classifier.random_forest import RandomForestClassifier
+from apps.ml.income_classifier.extra_trees import ExtraTreesClassifier
 
 try:
     registry = MLRegistry()
@@ -19,6 +20,18 @@ try:
     owner = "Ayo",
     algorithm_description = "Random Forest with sample pre and post-processing",
     algorithm_code=inspect.getsource(RandomForestClassifier))
+
+
+    et = ExtraTreesClassifier()
+    registry.add_algorithm(endpoint_name="income_classifier",
+    algorithm_object=et,
+    algorithm_name="extra trees",
+    algorithm_status="testing",
+    algorithm_version="0.0.1",
+    owner="Ayo",
+    algorithm_description="Extra Trees with simple pre and post-processing",
+    algorithm_code=inspect.getsource(ExtraTreesClassifier)
+    )
 
 except Exception as e:
     print("Exception while loading the algorithm to the registry,", str(e))
